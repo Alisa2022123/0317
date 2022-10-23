b6163be4766aa7b67d9d1de07c6a9aeb6afc7e39
Alisa2022123 <yli122116@gmail.come>
2022-10-23 09:58:35 +0000

    new-file

---
+++ a3/file.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#echo ---1.txt---
+#tac ./a3/1.txt
+
+#echo ######## file.text is opened ########
+#set file_1="export/home/caratins/trial/file.txt"
+#while read -r line
+#do
+#echo "$line" 
+#cp ./a3/1.txt ./a3/5.txt
+
+#done<./a3/1.txt
+#sort -r ./a3/5.txt
+tac ./a3/1.txt > ./a3/5.txt
